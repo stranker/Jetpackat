80ea0e726310abc1282097a44a35cdfe9225068e
Unknown <dani_mrx93@hotmail.com>
2019-04-01 22:08:55 -0300

    Add magnet area to player

--- Object/Collectable/Coin.gd
+++ Object/Collectable/Coin.gd
@@ -1,13 +1,20 @@
 extends Area2D
 
 var taked : bool = false
+var player_magnet = null
 export var value : int = 1
+export var speed : int = 300
 
 signal taken(val)
 
 func _ready():
 	randomize()
 	connect('taken',get_tree().root.get_node('GameScene'),'add_coin')
+
+func _process(delta):
+	if player_magnet:
+		var direction : Vector2 = (player_magnet.global_position - global_position).normalized()
+		translate(direction*speed*delta)
 
 func _on_Coin_body_entered(body):
 	if body.is_in_group('Player') and !taked:
@@ -20,4 +27,10 @@ func _on_Coin_body_entered(body):
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
+	pass # Replace with function body.
+
+
+func _on_Coin_area_entered(area):
+	if area.name == 'MagnetArea':
+		player_magnet = area
 	pass # Replace with function body.
