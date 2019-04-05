82ed26e47152175f7f50bbc556bbd6b8441982fd
Unknown <dani_mrx93@hotmail.com>
2019-04-05 19:40:45 -0300

    Fix if no item spawned doesn't crash

--- Spawner.gd
+++ Spawner.gd
@@ -22,20 +22,20 @@ func check_can_spawn():
 	return GameManager.player_height > height_spawn + pos_player_last_spawn
 
 func spawn_item(item,type):
-	assert(item!=null)
-	match type:
-		SPAWN_TYPE.COLLECTABLE:
-			get_tree().root.get_node('GameScene/Collectables').call_deferred('add_child',item)
-		SPAWN_TYPE.ENEMY:
-			get_tree().root.get_node('GameScene/Enemies').call_deferred('add_child',item)
-		SPAWN_TYPE.ORBIT:
-			get_tree().root.get_node('GameScene/Orbits').call_deferred('add_child',item)
-	var new_pos = Vector2()
-	new_pos.x = rand_range(100,get_viewport().get_visible_rect().size.x - 100)
-	new_pos.y = GameManager.camera.global_position.y - get_viewport().get_viewport().size.y / 2 - 200
-	item.position = new_pos
-	pos_player_last_spawn = GameManager.player_height
-	get_new_height()
+	if item:
+		match type:
+			SPAWN_TYPE.COLLECTABLE:
+				get_tree().root.get_node('GameScene/Collectables').call_deferred('add_child',item)
+			SPAWN_TYPE.ENEMY:
+				get_tree().root.get_node('GameScene/Enemies').call_deferred('add_child',item)
+			SPAWN_TYPE.ORBIT:
+				get_tree().root.get_node('GameScene/Orbits').call_deferred('add_child',item)
+		var new_pos = Vector2()
+		new_pos.x = rand_range(100,get_viewport().get_visible_rect().size.x - 100)
+		new_pos.y = GameManager.camera.global_position.y - get_viewport().get_viewport().size.y / 2 - 200
+		item.position = new_pos
+		pos_player_last_spawn = GameManager.player_height
+		get_new_height()
 	pass
 
 func set_active(val):
