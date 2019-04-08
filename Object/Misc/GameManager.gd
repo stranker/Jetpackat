7c732a6f1bb8f041c85a0b278ae4c047a7afefac
Unknown <dani_mrx93@hotmail.com>
2019-04-08 14:21:30 -0300

    Add buy item behavior

--- Object/Misc/GameManager.gd
+++ Object/Misc/GameManager.gd
@@ -1,7 +1,7 @@
 extends Node
 
-var coins : int = 0
-var fishes : int = 0
+var coins : int = 9999
+var fishes : int = 9999
 var playing : bool = false
 var player_height : int = 0
 var camera : Camera2D = null
