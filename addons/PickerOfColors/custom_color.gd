10d095e86538deb09a80e0ffbd9f5f1df8799599
Unknown <dani_mrx93@hotmail.com>
2019-05-29 17:27:16 -0300

    Add color picker of bitwes on game

---
+++ addons/PickerOfColors/custom_color.gd
@@ -0,0 +1,38 @@
+# #############################################################################
+# #############################################################################
+extends Control
+tool 
+var ColorWheel = load('res://addons/PickerOfColors/color_wheel.gd')
+var _color = Color(1,1,1)
+export var size : int
+var _color_wheel
+
+signal value_changed(c)
+
+func _ready():
+	_color_wheel = ColorWheel.new(size/11, 5)
+	set_selected_color(Color(.5, .5, .5))
+
+	_color_wheel.set_position(Vector2(size,size)*0.5)
+	add_child(_color_wheel)
+	_color_wheel.connect('selected', self, '_on_wheel_selected')
+	_color_wheel.set_index(0)
+	_color_wheel.set_value(1)
+	update()
+
+func _on_wheel_selected(color):
+	set_selected_color(color)
+	update()
+	emit_signal("value_changed", color)
+
+func set_selected_color(color):
+	_color = color
+	_color_wheel.set_color(color)
+	update()
+
+func get_selected_color():
+	return _color
+
+func _on_SelectButton_button_down():
+	hide()
+	pass # Replace with function body.
