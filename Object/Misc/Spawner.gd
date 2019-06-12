6c3be4815dcc6455e5b0a327ed977534ea04ab91
Daniel <danielnatarelli@gmail.com>
2019-06-12 06:48:13 -0300

    Rework UI, Add shield to cat and remove unused sprites

--- Object/Misc/Spawner.gd
+++ Object/Misc/Spawner.gd
@@ -22,16 +22,17 @@ func get_new_height():
 func check_can_spawn(): 
 	return GameManager.player_height > height_spawn + pos_player_last_spawn
 
-func spawn_item(item_scene : PackedScene):
+func spawn_item(item_scene):
+	var item = null
 	if !item_scene:
 		return
-	var item = item_scene.instance()
+	item = item_scene.instance()
+	get_node(parent_node).call_deferred('add_child',item)
 	var new_pos = Vector2()
 	spawn_points.shuffle()
-	get_node(parent_node).call_deferred('add_child',item)
 	new_pos.x = spawn_points[0].position.x
-	new_pos.y = GameManager.camera.global_position.y - get_viewport().get_viewport().size.y / 2 - 200
-	item.global_position = new_pos
+	new_pos.y = GameManager.camera.global_position.y - get_viewport().get_viewport().size.y
+	item.call_deferred('set_global_position',new_pos)
 	pos_player_last_spawn = GameManager.player_height
 	get_new_height()
 	pass
