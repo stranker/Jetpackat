595095463d4d2ba16f59d62fc29776ea17949831
Unknown <dani_mrx93@hotmail.com>
2019-09-13 08:45:28 -0300

    Fix small bugs

--- Scenes/BaseButtonShop.gd
+++ Scenes/BaseButtonShop.gd
@@ -1,9 +1,26 @@
 extends TextureButton
 
+var clicked = false
+
 func unclick_button():
+	$Anim.stop()
 	$Anim.play('Idle')
+	rect_scale = Vector2(1,1)
+	clicked = false
 	pass
 
 func _on_ButtonShop_button_down():
-	$Anim.play('Clicked')
+	if !clicked:
+		$Anim.play('Clicked')
+		clicked = true
+	pass # Replace with function body.
+
+
+func _on_ButtonShop_mouse_entered():
+	rect_scale = Vector2(1.2,1.2)
+	pass # Replace with function body.
+
+
+func _on_ButtonShop_mouse_exited():
+	rect_scale = Vector2(1,1)
 	pass # Replace with function body.
